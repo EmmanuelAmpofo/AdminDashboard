3b5cc7a2bdc3a35beadff100243c2bde1906bba6
EmmanuelAmpofo <emmanuel.ampofo@amalitech.org>
2022-08-12 14:29:41 +0000

    Card content component

--- src/utils/reusables/maincard/CardContent.jsx
+++ src/utils/reusables/maincard/CardContent.jsx
@@ -1,9 +1,15 @@
 import { Box } from '@material-ui/core'
 import React from 'react'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme)=>({
+
+}))
 
 function CardContent({children}) {
+  const classes =  useStyles()
   return (
-    <Box>
+    <Box className={classes.cardChildrenWrapper}>
       {children}
     </Box>
   )

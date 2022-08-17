08f443da1428e7b4fd160238223bdf78ef9f3698
EmmanuelAmpofo <emmanuel.ampofo@amalitech.org>
2022-08-17 13:57:09 +0000

    Graph chart layout

--- src/pages/dashboard/revenuegrapgh/Graph.jsx
+++ src/pages/dashboard/revenuegrapgh/Graph.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import MainCard from '../../../utils/reusables/maincard/MainCard'
+import Chart from './chart/Chart'
 
 function Graph() {
   return (
     <div>
-        <MainCard title="Last 5 Months"/>
+        <MainCard title="Last 5 Months" children={<Chart/>}/>
     </div>
   )
 }
